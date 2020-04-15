e225b6806febe2f58c44d538f8463c4f7a2d5f99
ThunderXBlitZ <goh_jia_wei@hotmail.com>
2020-04-15 14:03:06 +0800

    Refactor helper.py, added PCA, MDS and simple pairwise distance functions

--- lib/utils/distance_helper.py
+++ lib/utils/distance_helper.py
@@ -1,3 +1,5 @@
+# (Helper) functions for calculating pairwise distance for various compressions e.g. MDS, PCA
+
 from itertools import product, islice, chain
 import math
 import multiprocessing as mp
@@ -6,6 +8,8 @@ import time
 
 import h5py
 import numpy as np
+from sklearn.decomposition import PCA, IncrementalPCA, TruncatedSVD
+from sklearn.manifold import MDS
 from tqdm import tqdm
 
  
@@ -15,8 +19,29 @@ def _chunks(iterable, size=10):
         yield chain([first], islice(iterator, size - 1))
 
 
-def compute_pairswise_distance(X, num_samples:int, temp_filename:str, num_chunks:int = 100, del_tempfile:bool = False):
-    # num_samples: number of samples in X
+def compute_simple_pairwise_distance(X):
+    # suitable for toy datasets, memory increases exponentially for large datasets
+    distances = np.square(X[None, :, :] - X[:, None, :]).sum(-1)
+    return X
+
+
+def compute_original_pairwise_distance(X, num_samples:int, temp_filename:str, num_chunks:int = 100, del_tempfile:bool = False):
+    _generator = product(X, repeat=2)
+    distances = pairswise_distance(_generator, num_samples, temp_filename, num_chunks, del_tempfile)
+    return distances
+
+
+def compute_pca_pairwise_distance(X, num_samples:int, temp_filename:str, num_chunks:int = 100, del_tempfile:bool = False):
+    pca = PCA(n_components=4).fit(X)
+    # pca = IncrementalPCA(n_components=4, batch_size=batch_size).fit(X)
+    X_pca = pca.inverse_transform(pca.transform(X))
+    _generator = product(X_pca, repeat=2)
+    pca_distances = pairswise_distance(_generator, num_samples, temp_filename, num_chunks, del_tempfile)
+    return pca_distances
+
+
+def pairswise_distance(_iterator, num_samples:int, temp_filename:str, num_chunks:int = 100, del_tempfile:bool = False):
+    # num_samples: number of samples
     # temp_filename: filename for temp HDF5 file created
     # num_chunks: number of chunks to split dataset into for processing per worker 
     # del_tempfile: whether to delete temp HDF5 file afterwards
@@ -28,11 +53,9 @@ def compute_pairswise_distance(X, num_samples:int, temp_filename:str, num_chunks
         print("HDF5 file {filename} for pairwise distance already exists, skipping this step!")
     else:
         print(f"Generating pairwise distance, writing to HDF5 file {filename} ...")
-        _generator = product(X, repeat=2)
         chunk_size = math.ceil(num_samples**2 / num_chunks)
-
         h5f = h5py.File(filename, 'a')
-        for _chunk in tqdm(_chunks(_generator, chunk_size), total=num_chunks):
+        for _chunk in tqdm(_chunks(_iterator, chunk_size), total=num_chunks):
             distances = np.square([np.subtract(x[0], x[1]) for x in _chunk]).sum(-1)
             if "dataset" not in h5f:
                 h5f_dataset = h5f.create_dataset('dataset', data=distances, compression="gzip", chunks=True, maxshape=(None, )) 
@@ -46,7 +69,8 @@ def compute_pairswise_distance(X, num_samples:int, temp_filename:str, num_chunks
         distances = infile['dataset'][:]
     distances = distances.reshape(num_samples, num_samples)
     
-    if del_tempfile and os.path.exists(filename): os.remove(filename)
+    if del_tempfile and os.path.exists(filename):
+        os.remove(filename)
 
     # _elapsedTime = (time.time() - _start) / 60
     # print("Total time taken: ", _elapsedTime)
@@ -58,3 +82,10 @@ def compute_pairswise_distance(X, num_samples:int, temp_filename:str, num_chunks
         # 3 hr training time?
     # 60000 samples **2 will take 36 times longer i.e. 12hr
         # probably 12.6gb HDF5 file, 14,4gb RAM
+
+
+def mds_pairwise_distance(X, n_comp:int = 4, n_jobs: int = -1):
+    mds = MDS(n_components=4, random_state=42, n_jobs=-1, dissimilarity='euclidean')
+    X_mds = mds.fit_transform(X)
+    mds_distances = np.square(X_mds[None, :, :] - X_mds[:, None, :]).sum(-1) ** 0.5
+    return mds_distances
